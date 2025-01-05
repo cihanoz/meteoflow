6cbfa6af4d4f54597ad4891efd54503572a761c8
Cihan Özeren <cihanozeren@gmail.com>
2025-01-05 19:17:45 +0300

    Update Svelte configuration to use adapter-static and add it as a devDependency

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-  kit: {
-    adapter: adapter()
-  },
-  preprocess: vitePreprocess()
+import adapter from '@sveltejs/adapter-static';
+
+export default {
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: "index.html",
+			precompress: false,
+			strict: true
+		})
+	}
 };
-export default config;
